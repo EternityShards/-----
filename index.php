f2277ac2f37e87430cfdb52beea64e2da10a174b
EternityShards <162949914+EternityShards@users.noreply.github.com>
2024-03-11 13:12:41 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$number1 = 10;
+$number2 = 5;
+$number3 = 8;
+
+$maxNumber = max($number1, $number2, $number3);
+$minNumber = min($number1, $number2, $number3);
+
+echo "Max number: $maxNumber<br>";
+echo "Min number: $minNumber";
+?>
+
